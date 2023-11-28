9dabbde8c4f041679f34a6a5a6c74ed4132653c1
sheikhtazuddin <tazahmedsoft@gmail.com>
2023-11-28 14:17:43 -0800

    apply coupon code

--- src/Pages/DashBoard/Member/MakePayment/CheckoutForm.jsx
+++ src/Pages/DashBoard/Member/MakePayment/CheckoutForm.jsx
@@ -1,23 +1,56 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import { useEffect, useState } from 'react';
 import useAxiosSecure from '../../../../Hooks/AxiosSecure/useAxiosSecure';
+import useCupon from '../../../../Hooks/useCupon/useCupon';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 
 const CheckoutForm = ({ payment }) => {
     const axiosSecure = useAxiosSecure()
+    const [cupon] = useCupon()
     const amount = payment?.rent
     const [error, setError] = useState('')
     const [paymentId, setPaymentId] = useState('')
     const stripe = useStripe();
     const elements = useElements();
+    const [totalPay, setTotalPay] = useState(amount)
     // console.log(payment)
     const [clientSecret, setClientSecret] = useState('')
+    const navagate = useNavigate()
+    const handleCoupon = (e) => {
+        e.preventDefault()
+        const applyCoupon = e.target.coupon.value;
+        const findcoupon = cupon.find(item => item.cuponCode === applyCoupon)
+        if (findcoupon && !clientSecret) {
+            const discountPrice = parseInt(findcoupon?.discount)
+            const discountAmount = (discountPrice * amount) / 100
+            const newPrice = amount - discountAmount
+            setTotalPay(newPrice)
+            e.target.reset()
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "Coupon apply success",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        } else {
+            Swal.fire({
+                position: "center",
+                icon: "warning",
+                title: "Enter a vaild Coupon",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+    }
     useEffect(() => {
-        axiosSecure.post('/create-payment-intent', { price: amount })
+        axiosSecure.post('/create-payment-intent', { price: totalPay })
             .then(res => {
                 console.log(res?.data?.clientSecret)
                 setClientSecret(res?.data?.clientSecret)
             })
-    }, [amount, axiosSecure])
+    }, [totalPay, axiosSecure])
     const handleSubmit = async (event) => {
         // Block native form submission.
         event.preventDefault();
@@ -58,9 +91,9 @@ const CheckoutForm = ({ payment }) => {
                     billing_details: {
                         name: payment?.userName || "name",
                         email: payment?.userEmail || 'email',
-                        
+
                     },
-                    
+
                 },
             },
         );
@@ -69,37 +102,59 @@ const CheckoutForm = ({ payment }) => {
         } else {
             console.log("success:", paymentIntent)
             if (paymentIntent?.status === 'succeeded') {
-                alert("success")
+                // alert("success")
                 setPaymentId(paymentIntent?.id)
-               
-                    
-                    
-                    
-                const paymentInfo={
-                    userName:payment?.userName,
-                    userEmail:payment?.userEmail,
-                    requetsId:payment?.requetsId,
-                    floorNo:payment?.floorNo,
-                    blockName:payment?.blockName,
-                    apartmentNo:payment?.apartmentNo,
-                    RoomNo:payment?.RoomNo,
-                    amount:(paymentIntent?.amount)/100,
-                    Date:new Date().toLocaleDateString('en-US', {
+
+
+
+
+                const paymentInfo = {
+                    userName: payment?.userName,
+                    userEmail: payment?.userEmail,
+                    requetsId: payment?.requetsId,
+                    floorNo: payment?.floorNo,
+                    blockName: payment?.blockName,
+                    apartmentNo: payment?.apartmentNo,
+                    RoomNo: payment?.RoomNo,
+                    amount: (paymentIntent?.amount) / 100,
+                    Date: new Date().toLocaleDateString('en-US', {
                         month: '2-digit',
                         day: '2-digit',
                         year: 'numeric',
-                      })
+                    })
                 }
-                axiosSecure.post('/paymentHistory',paymentInfo)
-                .then(res=>{
-                    console.log(res.data)
-                })
+                console.log(paymentInfo)
+                axiosSecure.post('/paymentHistory', paymentInfo)
+                    .then(res => {
+                        // console.log(res.data)
+                        if (res?.data?.insertedId) {
+                            Swal.fire({
+                                position: "center",
+                                icon: "success",
+                                title: "Thank You For The Payment",
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                            navagate('/dashboard/paymentHistory')
+                        }
+                    })
             }
         }
     };
     return (
         <div className='px-10 ' >
             <div className='w-1/2 mx-auto mt-10 bg-[#0e0a267f] px-10 py-5 rounded-2xl'>
+                <div>
+                    <h1 className='text-xl text-white text-center'>Your Payment Amount is : <span className='bg-blue-900
+                    rounded-xl py-2 px-3'>{totalPay}</span></h1>
+                    <form onSubmit={handleCoupon}>
+                        <div className='flex items-center justify-center mt-5' >
+                            <input type="text" className='w-2/3 p-3 rounded-l-xl pl-5' placeholder='Enter Coupon' name='coupon' />
+                            <button className='py-[10px] px-5 rounded-r-xl bg-primary text-white font-sans font-semibold text-xl ' type='submit'>Apply</button>
+                        </div>
+                    </form>
+
+                </div>
                 <form onSubmit={handleSubmit} className=''>
                     <CardElement
                         options={{
