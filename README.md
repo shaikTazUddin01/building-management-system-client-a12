425f3e5a19b1b5ed1ff9c23f9f061999b2d37259
sheikhtazuddin <tazahmedsoft@gmail.com>
2023-11-30 10:51:12 -0800

    rechecked and updated some content

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Live Link : [https://cool-sorbet-7b3b40.netlify.app](https://cool-sorbet-7b3b40.netlify.app) 
 
+#About This Project 
+
+- It is a Building Manage Application
+
+- Any user able to agreement for any room
+
+- When user sent a agreement request then Admin see this request
+
+- If Admin accept this request then he/she is the member of this house
+
+- If Admin removed his/her member ship then he can not access member profile
+
+- In member Profile he/she payment for his/her rented room
+
+- Member Also see his Payment History in Payment History page
+
 
 
 
--- src/Component/Home/GoogleMap/GoogleMap.jsx
+++ src/Component/Home/GoogleMap/GoogleMap.jsx
@@ -9,8 +9,10 @@ const GoogleMap = () => {
                 <Divider header={'Building Location'}></Divider>
                 <div className="flex flex-col lg:flex-row gap-5 items-center mt-8">
                     <div className="gmap_canvas mt-5 w-full lg:w-1/2" data-aos="zoom-in-down">
-                     
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.7328109202886!2d90.40437737429404!3d23.79252708715977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c78024765f8d%3A0x5bca8a74f569a62c!2sBanani%20House!5e0!3m2!1sen!2sbd!4v1701229754338!5m2!1sen!2sbd" frameBorder="0" scrolling="no" className=" h-[300px] md:h-[500px] w-[94%] border-2 mx-auto border-blue-500 "></iframe>
+                    
+                        <iframe src="https://maps.google.com/maps?q=House%2020%20Road%20104,%20Gulshan,%20Dhaka%201212&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameBorder="0" scrolling="no" className=" h-[300px] md:h-[500px] w-[94%] border-2 mx-auto border-blue-500 "></iframe>
+                       
+                        {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.7328109202886!2d90.40437737429404!3d23.79252708715977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c78024765f8d%3A0x5bca8a74f569a62c!2sBanani%20House!5e0!3m2!1sen!2sbd!4v1701229754338!5m2!1sen!2sbd" frameBorder="0" scrolling="no" className=" h-[300px] md:h-[500px] w-[94%] border-2 mx-auto border-blue-500 "></iframe> */}
                     </div>
 
                     <div className="w-full lg:w-1/2">
